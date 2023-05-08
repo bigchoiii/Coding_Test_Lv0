6e89921d7f77752ac3762dddfd76ac3050dd0f19
bigchoiii <poc686kr@gmail.com>
2023-05-08 23:48:03 +0900

    First commit

---
+++ 수 조작하기2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Solution
+{
+    public string solution(int[] numLog) {
+        string answer = "";
+
+        for (int i = 0; i < numLog.Length - 1; i++) {
+            if (numLog[i + 1] - numLog[i] == 1) {
+                answer += "w";
+            } else if (numLog[i + 1] - numLog[i] == -1) {
+                answer += "s";
+            } else if (numLog[i + 1] - numLog[i] == 10) {
+                answer += "d";
+            } else {
+                answer += "a";
+            }
+        }
+        return answer;
+    }
+}
